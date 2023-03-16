4d78aee1a5e0136e6533595340bf2b110fbccbcd
Paul-Crp <Paulcrespin159@gmail.com>
2023-03-16 12:53:21 +0100

    Update Démine.R
    Amélioration de la clareté du code, grille rectangulaire supportée, logique des drapeaux au début de partie finalisée

--- sources/Démine.R
+++ sources/Démine.R
@@ -4,15 +4,15 @@
 afficher_grille <- function(grille, visible, drapeaux) {
   for (i in 1:nrow(grille)) {
     for (j in 1:ncol(grille)) {
-      if (visible[i,j]) {
-        if (grille[i,j] == -2) {
+      if (visible[i, j]) {
+        if (grille[i, j] == -2) {
           cat("F ")
-        } else if (grille[i,j] == -1) {
+        } else if (grille[i, j] == -1) {
           cat("* ")
         } else {
-          cat(grille[i,j], "")
+          cat(grille[i, j], "")
         }
-      } else if (drapeaux[i,j]) {
+      } else if (drapeaux[i, j]) {
         cat("D ")
       } else {
         cat("# ")
@@ -27,10 +27,10 @@ afficher_grille <- function(grille, visible, drapeaux) {
 ## Fonction pour calculer le nombre de mines adjacentes
 calculer_mines_adjacentes <- function(grille, i, j) {
   nb_mines_adjacentes <- 0
-  for (k in (i-1):(i+1)) {
-    for (l in (j-1):(j+1)) {
-      if (k >= 1 & k <= nrow(grille) & l >= 1 & l <= ncol(grille)) {
-        if (grille[k,l] == -1) {
+  for (k in (i - 1):(i + 1)) {
+    for (l in (j - 1):(j + 1)) {
+      if (k >= 1 && k <= nrow(grille) && l >= 1 && l <= ncol(grille)) {
+        if (grille[k, l] == -1) {
           nb_mines_adjacentes <- nb_mines_adjacentes + 1
         }
       }
@@ -41,16 +41,16 @@ calculer_mines_adjacentes <- function(grille, i, j) {
 
 ## Fonction pour révéler les cases adjacentes vides
 reveler_cases_adjacentes <- function(grille, visible, i, j) {
-  if (visible[i,j]) {
+  if (visible[i, j]) {
     return(visible)
   }
-  visible[i,j] <- TRUE
-  if (grille[i,j] != 0) {
+  visible[i, j] <- TRUE
+  if (grille[i, j] != 0) {
     return(visible)
   }
-  for (k in (i-1):(i+1)) {
-    for (l in (j-1):(j+1)) {
-      if (k >= 1 & k <= nrow(grille) & l >= 1 & l <= ncol(grille)) {
+  for (k in (i - 1):(i + 1)) {
+    for (l in (j - 1):(j + 1)) {
+      if (k >= 1 && k <= nrow(grille) && l >= 1 && l <= ncol(grille)) {
         visible <- reveler_cases_adjacentes(grille, visible, k, l)
       }
     }
@@ -58,149 +58,160 @@ reveler_cases_adjacentes <- function(grille, visible, i, j) {
   return(visible)
 }
 
-# Données
-
-
-## Définition de la taille de la grille
-taille_grille <- 10
-
-## Définition du nombre de mines
-nb_mines <- 10
-
 # Jouer
 
 jouer_partie <- function() {
-  
-  choix2 <- readline("Choisissez une taille de grille et le nombre de mines (par exemple, 10 10): ")
+
+  choix2 <- readline("Choisissez une taille de grille et le nombre de mines (par ex, 10espace5espace10): ")
   choix2 <- strsplit(choix2, " ")[[1]]
-  taille_grille <- as.integer(choix2[1])
-  nb_mines <- as.integer(choix2[2])
-  
+  taille_grille1 <- as.integer(choix2[1])
+  taille_grille2 <- as.integer(choix2[2])
+  nb_mines <- as.integer(choix2[3])
+
   ## Création de la grille vide
-  grille <- matrix(0, nrow = taille_grille, ncol = taille_grille)
-  
+  grille <- matrix(0, nrow = taille_grille1, ncol = taille_grille2)
+
   ## Placement des bombes de manière aléatoire
-  mines <- sample(1:(taille_grille*taille_grille), nb_mines)
+  mines <- sample(1:(taille_grille1 * taille_grille2), nb_mines)
   grille[mines] <- -1
-  
+
   visible <- matrix(FALSE, nrow = nrow(grille), ncol = ncol(grille))
   drapeaux <- matrix(FALSE, nrow = nrow(grille), ncol = ncol(grille))
-  
+
   afficher_grille(grille, visible, drapeaux)
-  choix <- readline("Choisissez une case (par exemple, 3 4): ")
+  choix <- readline("Choisissez une case (par exemple, 4espace2): ")
   choix <- strsplit(choix, " ")[[1]]
   i <- as.integer(choix[1])
   j <- as.integer(choix[2])
   d <- choix[3]
-  if (is.na(d)){
-    A=TRUE
-    if (grille[i,j] ==-1){
-      A=FALSE
+  ptm <- proc.time()
+  if (is.na(d)) {
+    test <- TRUE
+    if (grille[i, j] == -1) {
+      test <- FALSE
     }
-  
-    while (!A) {
-      grille <- matrix(0, nrow = taille_grille, ncol = taille_grille)
-      mines <- sample(1:(taille_grille*taille_grille), nb_mines)
-      grille[mines] <- -1
-      if (grille[i,j] !=-1){
-        A=TRUE
-      }
-    }
-  
-    for (ii in 1:nrow(grille)) {
-      for (jj in 1:ncol(grille)) {
-        if (grille[ii,jj]!= -1) {
-          grille[ii,jj] <- calculer_mines_adjacentes(grille, ii, jj)
-        }
-      }
-    }
-  
-    if (is.na(d)){
-      visible <- reveler_cases_adjacentes(grille,visible,i,j)
-    } else {drapeaux[i,j] <- !drapeaux[i,j]}
-  } else {while (!is.na(d)){
-    drapeaux[i,j] <- !drapeaux[i,j]
-    afficher_grille(grille, visible, drapeaux)
-    d=NA
-    choix <- readline("Choisissez une case (par exemple, 3 4): ")
-    choix <- strsplit(choix, " ")[[1]]
-    i <- as.integer(choix[1])
-    j <- as.integer(choix[2])
-    d <- choix[3]
-  }
-    A=TRUE
-    if (grille[i,j] ==-1){
-      A=FALSE
-    }
-    
-    while (!A) {
-      grille <- matrix(0, nrow = taille_grille, ncol = taille_grille)
-      mines <- sample(1:(taille_grille*taille_grille), nb_mines)
-      grille[mines] <- -1
-      if (grille[i,j] !=-1){
-        A=TRUE
-      }
-    }
-    
-    for (ii in 1:nrow(grille)) {
-      for (jj in 1:ncol(grille)) {
-        if (grille[ii,jj]!= -1) {
-          grille[ii,jj] <- calculer_mines_adjacentes(grille, ii, jj)
-        }
-      }
-    }
-    visible <- reveler_cases_adjacentes(grille,visible,i,j)
-    
-  }
-  
 
-  ### On fait une première boucle à part pour ne pas perdre au premier coup
-  
-  partie_terminee <- FALSE
-  
-  while (!partie_terminee) {
-    ptm <- proc.time()
-    afficher_grille(grille, visible, drapeaux)
-    nb_drapeaux=nb_mines-sum(drapeaux)
-    if (sum(visible)==taille_grille^2-nb_mines) {
-      cat("Gagné !")
-      cat("Vous avez gagné en: ")
-      cat((proc.time()-ptm)[3])
-      cat(" s")
-      partie_terminee <- TRUE
+    while (!test) {
+      grille <- matrix(0, nrow = taille_grille1, ncol = taille_grille2)
+      mines <- sample(1:(taille_grille1 * taille_grille2), nb_mines)
+      grille[mines] <- -1
+      if (grille[i, j] != -1) {
+        test <- TRUE
+      }
+    }
+
+    for (ii in 1:nrow(grille)) {
+      for (jj in 1:ncol(grille)) {
+        if (grille[ii, jj] != -1) {
+          grille[ii, jj] <- calculer_mines_adjacentes(grille, ii, jj)
+        }
+      }
+    }
+
+    if (is.na(d)) {
+      visible <- reveler_cases_adjacentes(grille, visible, i, j)
     } else {
-      choix <- readline("Choisissez une case (par exemple, 3 4): ")
+      drapeaux[i, j] <- !drapeaux[i, j]
+    }
+  } else {
+    drapeaux[i, j] <- !drapeaux[i, j]
+    while (!is.na(d)) {
+      nb_drapeaux <- nb_mines - sum(drapeaux)
+      afficher_grille(grille, visible, drapeaux)
+      d <- NA
+      choix <- readline("Choisissez une case (par exemple, 4espace2): ")
       choix <- strsplit(choix, " ")[[1]]
       i <- as.integer(choix[1])
       j <- as.integer(choix[2])
       d <- choix[3]
-      if (!is.na(d)){
-        if (sum(drapeaux)==nb_mines){
+      if (!is.na(d)) {
+        if (nb_drapeaux == 0) {
           cat("Vous n'avez plus de drapeaux...")
           cat("\n")
-        } else if (visible[i,j]) {
+      } else {
+        drapeaux[i, j] <- !drapeaux[i, j]
+      }
+    } else if (drapeaux[i, j]) {
+        cat("Il y a un drapeau ici")
+        cat("\n")
+        d <- 5
+      }
+  }
+
+    test <- TRUE
+    if (grille[i, j] == -1) {
+      test <- FALSE
+    }
+
+    while (!test) {
+      grille <- matrix(0, nrow = taille_grille1, ncol = taille_grille2)
+      mines <- sample(1:(taille_grille1 * taille_grille2), nb_mines)
+      grille[mines] <- -1
+      if (grille[i, j] != -1) {
+        test <- TRUE
+      }
+    }
+
+    for (ii in 1:nrow(grille)) {
+      for (jj in 1:ncol(grille)) {
+        if (grille[ii, jj] != -1) {
+          grille[ii, jj] <- calculer_mines_adjacentes(grille, ii, jj)
+        }
+      }
+    }
+    visible <- reveler_cases_adjacentes(grille, visible, i, j)
+  }
+
+  ### On fait une première boucle à part pour ne pas perdre au premier coup
+
+  partie_terminee <- FALSE
+  while (!partie_terminee) {
+    afficher_grille(grille, visible, drapeaux)
+    nb_drapeaux <- nb_mines - sum(drapeaux)
+    if (sum(visible) == taille_grille1 * taille_grille2 - nb_mines) {
+      cat("Gagné ! ")
+      cat("\n")
+      cat("Vous avez gagné en: ")
+      cat((proc.time() - ptm)[3])
+      cat(" s")
+      cat("\n")
+      partie_terminee <- TRUE
+    } else {
+      choix <- readline("Choisissez une case (par exemple, 4espace2): ")
+      choix <- strsplit(choix, " ")[[1]]
+      i <- as.integer(choix[1])
+      j <- as.integer(choix[2])
+      d <- choix[3]
+      if (!is.na(d)) {
+        if (nb_drapeaux == 0) {
+          cat("Vous n'avez plus de drapeaux...")
+          cat("\n")
+        } else if (visible[i, j]) {
           cat("Vous avez déjà creusez ici...")
           cat("\n")
         } else {
-          drapeaux[i,j] <- !drapeaux[i,j]
+          drapeaux[i, j] <- !drapeaux[i, j]
         }
-      } else if(drapeaux[i,j]){
+      } else if (drapeaux[i, j]) {
         cat("Il y a un drapeau ici")
         cat("\n")
-      } else if (grille[i,j] == -1) {
+      } else if (grille[i, j] == -1) {
         cat("Perdu !")
         cat("\n")
         cat("Vous avez perdu en: ")
-        cat((proc.time()-ptm)[3])
+        cat((proc.time() - ptm)[3])
         cat(" s")
-        cat("\n")  
+        cat("\n")
         visible <- matrix(TRUE, nrow = nrow(grille), ncol = ncol(grille))
-        grille[i,j]=-2
-        afficher_grille(grille, visible ,drapeaux)
+        grille[i, j] <- -2
+        afficher_grille(grille, visible, drapeaux)
         partie_terminee <- TRUE
       } else {
-        visible <- reveler_cases_adjacentes(grille,visible,i,j)
-      }}}}
+        visible <- reveler_cases_adjacentes(grille, visible, i, j)
+      }
+    }
+  }
+}
 
 ### readline est indispensable, on ne peut pas mettre print, ni cat
 
