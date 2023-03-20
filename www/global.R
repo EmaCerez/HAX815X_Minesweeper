0f1088c27c432ea39a02bef2db6ef6a766bc35a5
Ema Cerezo <114211762+EmaCerez@users.noreply.github.com>
2023-03-20 15:45:17 +0100

    Doublon supprimé

--- www/global.R
+++
@@ -1,5 +0,0 @@
-
-# Interactif
-difficulty <- 3                   # Créer des boutons sur welcome et un menu
-flags_left <- bombs[difficulty]   # Faire que ça aug/dim selon drapeaux placés
-points <- 1800                    # Faire que ça augmente automatiquement
